05c703a02609381a3cb8831048481b8ee6ff9315
Faiyaz Tanvee <faiyaztanvee009@gmail.com>
2023-03-05 13:13:43 +0600

    Create Algorithm intro.cpp

---
+++ Algorithm intro.cpp
@@ -0,0 +1,41 @@
+//Suppose we want to cross a road. We look left and right over and over to do that.
+//it's a method. methods like this are called algortithm.
+//then, think about your math exam, you do different maths with different methods.
+//that's also designated as algorithm.
+/**
+An algorithm is a set of steps or instructions that are followed to solve a
+problem or complete a task. It is a sequence of actions that can be executed by
+a computer to accomplish a specific goal. In C++, algorithms are used to
+manipulate data structures like arrays, vectors, and strings.
+
+Think of an algorithm like a recipe for baking a cake. Just as a recipe lists
+out the necessary ingredients and steps to create a delicious cake, an algorithm
+lists out the necessary steps to solve a particular problem. Each step in the
+algorithm is like a separate instruction in the recipe. Just as following the
+recipe exactly will result in a perfect cake, following the algorithm precisely
+will result in a correct solution to the problem.
+
+For example, let's say you want to sort a list of numbers in ascending order.
+The algorithm for sorting can be broken down into a few simple steps:
+
+Compare the first two numbers in the list.
+If the first number is greater than the second number, swap their positions.
+Move on to the next two numbers and repeat step 2.
+Continue this process until you have compared and swapped all of the numbers in
+the list.
+
+Another example of an algorithm is finding the largest number in an array. The
+algorithm for finding the largest number can be broken down into the following
+steps:
+
+Start by assuming that the first number in the array is the largest.
+Compare this number with the next number in the array.
+If the next number is larger, update the largest number variable to be the next
+number.
+Repeat this process for all of the numbers in the array.
+Once you have gone through all of the numbers in the array, the largest number
+variable will contain the largest number.
+In summary, an algorithm is a sequence of steps used to solve a problem or
+complete a task. It can be thought of as a recipe that must be followed precisely
+in order to achieve the desired result.
+**/
