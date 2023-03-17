062de20d6c89b4d4bfd29d1a6171e49590a85771
Faiyaz Tanvee <faiyaztanvee009@gmail.com>
2023-03-17 18:34:29 +0600

    17/03/2023

--- Testing_program.cpp
+++ Testing_program.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    deque<int> dq;
-    dq.push_back(1);
-    dq.push_front(1);
-    dq.push_back(2);
-    dq.push_front(9);
-    dq.push_back(4);
-    while (!dq.empty())
-    {
-        cout << dq.front() << " ";
-        dq.pop_front();
-    }
+long long powpow (int num, int pw)
+{
+    if(pw==1) return num;
+    int pro = num*powpow(num, pw-1);
+    return pro;
+}
+
+int main()
+{
+    int n, p;
+    cin >> n >> p;
+    cout<<powpow(n,p);
     return 0;
 }
