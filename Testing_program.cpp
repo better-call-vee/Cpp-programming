ff684b59a8662475307a6ff111cf69fc44d2a761
Faiyaz Tanvee <faiyaztanvee009@gmail.com>
2023-07-16 14:43:14 +0600

    go

--- Testing_program.cpp
+++ Testing_program.cpp
@@ -1,33 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
-#define fast                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(nullptr);                 \
-    cout.tie(nullptr);
+using namespace std;
 
 int main()
 {
-    fast;
-
-    int n;
-    cin >> n;
-
-    vector<int> instability(n);
-
-    for (int i = 0; i < n; i++)
-        cin >> instability[i];
-
-    sort(instability.begin(), instability.end());
-
-    int comp1 = instability[n - 2] - instability[0];
-    int comp2 = instability[n - 1] - instability[1];
-
-    if (n > 2)
-        cout << min(comp1, comp2);
-
-    else
-        cout << 0;
-
-    return 0;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int need = (k - 1) / (n - 1);
+        cout << k + need << endl;
+    }
 }
