946cddd0f595254300b9bb736ee334ed28461568
Faiyaz Tanvee <faiyaztanvee009@gmail.com>
2023-10-18 23:12:39 +0600

    Update Number_Theories.cpp

--- Number_Theories.cpp
+++ Number_Theories.cpp
@@ -11,7 +11,7 @@ Given two integers x and y, and a range from 1 to n:
     Identifying Common Multiples of x and y:
         The common multiples of x and y within a given range are the multiples of their Least Common Multiple (LCM).
         The formula for LCM of two numbers aa and bb is:
-        LCM(a,b)=a×b/GCD(a,b)
+        LCM(a, b)= a × b / GCD(a, b)
 
         Here, GCD stands for the Greatest Common Divisor.
         The count of common multiples of x and y in the range is ⌊nLCM(x,y)⌋⌊LCM(x,y)n​⌋.
@@ -82,6 +82,21 @@ Sum=m / 2 × (N − m + 1 + N)
 Sum=m / 2 × (2N − m + 1)
 highSum = (k * (2 * n - k + 1)) / 2;
 */
+
+
+
+
+/*
+╔═══════════╗
+║ FORMULAS: ║
+╚═══════════╝
+
+We can find the sum of squares of the first n natural numbers using the formula, 
+SUM = 1^2 + 2^2 + 3^2 + ... + n^2 = [n(n+1)(2n+1)] / 6
+*/
+
+
+
 
 /*
 --------------------------------------------------------------------------------
@@ -184,9 +199,9 @@ suppose, 1,2,3 => here  {1,2} will be a combination, as well {2, 1} will be coun
 
 
 /*
---------------------------
-|       NOTATIONS        |
---------------------------
+------------------------------------------
+|       NOTATIONS and PROGRESSION        |
+------------------------------------------
 i=4
  ∏  i => 1×2×3×4=24   this is pie notation.
 i=1
@@ -194,6 +209,11 @@ i=1
 i=4
  ∑  i^2 => 1+4+9+16=30    this is sigma notation.
 i=1
+
+i=n-1
+ ∑  r^i => r^n - 1 / r - 1 . suppose, r = 2. and i will go till n. then => 2^(n+1) - 1/ 2 - 1.
+i=0
+this is geometric progression
 */
 
 
@@ -344,4 +364,181 @@ last 2's power for all this.
 
 Modulo World => suppose n mod 6, in this world 5, 11, 17, 23... belong to the same family. and so on like this.
 
+
+1) a≡b (mod m) means that a and b have the same remainder when divided by m. that is a mod m = b mod m.
+and for that (a-b) mod m = 0. 1 % 6 => 1 and 7 % 6 => 1. 7 - 1 = 6 mod 6 => 0.
+
+2) (a + b) ≡ (a mod m + b mod m) (mod m);
+3) (a - b) ≡ (a mod m - b mod m) (mod m);
+4) (a x b) = (a mod m x b mod m) (mod m);
+5) a^b ≡ (a mod m)^b (mod m);
+6) a-b if negative ≡ ((a mod m - b mod m) % m + m) % m;
+
+these don't work with a/b.
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a = 7;
+  int b = 6;
+  int m = 5;
+
+  // Property: (a + b) mod m = ((a mod m) + (b mod m)) mod m
+  int result1 = (a + b) % m;
+  int result2 = ((a % m) + (b % m)) % m;
+  cout << "(a + b) mod m = " << result1 << endl;
+  cout << "((a mod m) + (b mod m)) mod m = " << result2 << endl;
+
+  // Property: (a - b) mod m = ((a mod m) - (b mod m) + m) mod m
+  int result3 = (a - b) % m;
+  int result4 = ((a % m) - (b % m) + m) % m;
+  cout << "(a - b) mod m = " << result3 << endl;
+  cout << "((a mod m) - (b mod m) + m) mod m = " << result4 << endl;
+
+  // Property: (a * b) mod m = ((a mod m) * (b mod m)) mod m
+  int result5 = (a * b) % m;
+  int result6 = ((a % m) * (b % m)) % m;
+  cout << "(a * b) mod m = " << result5 << endl;
+  cout << "((a mod m) * (b mod m)) mod m = " << result6 << endl;
+
+  int c, d;
+  // a * b * c * d mod m = (((a * b) mod m) * c mod m) * d mod m
+  a = 1e9 - 3; b = 1e9 - 1; c = 1e9 - 2; d = 1e9 - 3; m = 1e9 + 7;
+  int result7 = (a * b * c * d) % m; // overflow
+  int result8 = (((a * b) % m) * c % m) * d % m; // still overflow
+  int result9 = (((1LL * a * b) % m) * c % m) * d % m; // no overflow
+  cout << "(a * b * c * d) mod m = " << result7 << ' ' << result8 << ' ' << result9 << endl;
+
+  // a - 3 * b mod m = (a mod m - (3 * b) mod m + m) mod m
+  cout << "(a - 3 * b) mod m = " << (a - 3LL * b % m + m) % m << endl;
+
+  // find n! mod m
+  int n = 100;
+  int fact = 1;
+  for (int i = 1; i <= n; i++) {
+    fact = (1LL * fact * i) % m;
+  }
+  cout << n << "! mod m = " << fact << endl;
+
+  // find a^b mod m
+  b = 100;
+  int power = 1;
+  for (int i = 1; i <= b; i++) {
+    power = (1LL * power * a) % m;
+  }
+  cout << a << "^" << b << " mod m = " << power << endl;
+  return 0;
+}
+*/
+
+
+
+
+
+/*
+╔═══════════════════════╗
+║ Some Builtin Function ║
+╚═══════════════════════╝
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // abs
+  int num_1 = -10;
+  int abs_result = abs(num_1);
+  cout << "Absolute value of " << num_1 << " is: " << abs_result << '\n';
+
+  // ceil, floor, round, llround
+  double num_2 = 3.7;
+  double ceil_result = ceil(num_2);
+  double floor_result = floor(num_2);
+  double round_result = round(num_2);
+  long long llround_result = llround(num_2);
+  cout << "Ceil value of " << num_2 << " is: " << ceil_result << '\n';
+  cout << "Floor value of " << num_2 << " is: " << floor_result << '\n';
+  cout << "Round value of " << num_2 << " is: " << round_result << '\n';
+  cout << "Long long rounded value of " << num_2 << " is: " << llround_result << '\n';
+
+  // log, log2, log10
+  double num_3 = 1000.0;
+  double log_result = log(num_3);
+  double log2_result = log2(num_3);
+  double log10_result = log10(num_3);
+  cout << "Natural logarithm of " << num_3 << " is: " << log_result << '\n';
+  cout << "Base-2 logarithm of " << num_3 << " is: " << log2_result << '\n';
+  cout << "Base-10 logarithm of " << num_3 << " is: " << log10_result << '\n';
+
+  // sqrt, cbrt, pow
+  double num_4 = 16.0;
+  double sqrt_result = sqrt(num_4);
+  double cbrt_result = cbrt(num_4);
+  double pow_result = pow(num_4, 2);
+  cout << "Square root of " << num_4 << " is: " << sqrt_result << '\n';
+  cout << "Cube root of " << num_4 << " is: " << cbrt_result << '\n';
+  cout << num_4 << " raised to the power of 2 is: " << pow_result << '\n';
+
+  // stoi, stoll, to_string
+  char s[] = "12345";
+  int stoi_result = stoi(s);
+  long long stoll_result = stoll(s);
+  cout << "String to integer conversion: " << stoi_result << '\n';
+  cout << "String to long long conversion: " << stoll_result << '\n';
+  cout << "Integer to string conversion: " << to_string(stoi_result) << '\n';
+
+  // cerr, cout
+  cerr << "This is an error message" << '\n';
+  cout << "This is a regular output" << '\n';
+
+  // isalpha, isdigit, islower, isupper, tolower, toupper
+  char ch = 'A';
+  bool is_alpha_result = isalpha(ch);
+  bool is_digit_result = isdigit(ch);
+  bool is_lower_result = islower(ch);
+  bool is_upper_result = isupper(ch);
+  char to_lower_result = tolower(ch);
+  char to_upper_result = toupper(ch);
+  cout << "Is " << ch << " an alphabet? " << is_alpha_result << '\n';
+  cout << "Is " << ch << " a digit? " << is_digit_result << '\n';
+  cout << "Is " << ch << " in lowercase? " << is_lower_result << '\n';
+  cout << "Is " << ch << " in uppercase? " << is_upper_result << '\n';
+  cout << "Lowercase conversion of " << ch << " is: " << to_lower_result << '\n';
+  cout << "Uppercase conversion of " << ch << " is: " << to_upper_result << '\n';
+
+  // swap
+  int num_5 = 10;
+  int num_6 = 20;
+  cout << "Before swapping: num_5 = " << num_5 << ", num_6 = " << num_6 << '\n';
+  swap(num_5, num_6);
+  cout << "After swapping: num_5 = " << num_5 << ", num_6 = " << num_6 << '\n';
+
+  // gcd
+  int num_7 = 36;
+  int num_8 = 48;
+  int gcd_result = __gcd(num_7, num_8);
+  cout << "GCD of " << num_7 << " and " << num_8 << " is: " << gcd_result << '\n';
+
+  // rand
+  srand(time(0)); // Seed the random number generator
+  int random_num = rand();
+  cout << "Random number: " << random_num << '\n';
+
+  // reverse
+  char reverse_str[] = "Hello, World!";
+  int len = strlen(reverse_str);
+  reverse(reverse_str, reverse_str + len);
+  cout << "Reversed string: " << reverse_str << '\n';
+
+  // sort, is_sorted
+  int a[] = {5, 2, 3, 1, 9};
+  int n = 5;
+  sort(a, a + n);
+  bool is_array_sorted = is_sorted(a, a + n);
+  cout << "Sorted numbers: ";
+  for (int i = 0; i < n; i++) {
+    cout << a[i] << " ";
+  }
+  cout << '\n';
+  cout << "Is the array sorted? " << is_array_sorted << '\n';
+  return 0;
+}
 */
